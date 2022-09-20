e5f3147664ad86b3aa039e1a41fc212647f07df0
AdlanBousehaba <adlan.bousehaba@epitech.eu>
2022-09-21 00:34:16 +0200

    try to make first style on text input

--- App.js
+++ App.js
@@ -1,14 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, ScrollView } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Home from './components/Home';
+import Login from './components/Login';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,3 +13,18 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+  return (
+    <>
+    <View>
+      <Login/>
+    </View>
+    </>
+  );
+}
+
